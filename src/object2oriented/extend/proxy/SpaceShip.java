1e2e03c24a37584a92870c80f48ed786dab69510
yuf <duanyufeii@qq.com>
2019-05-06 17:22:21 +0800

    上传面向对象和string代码

---
+++ src/object2oriented/extend/proxy/SpaceShip.java
@@ -0,0 +1,19 @@
+package object2oriented.extend.proxy;
+/**
+ * @Author: dyf
+ * @Date: 2019/5/5 11:00
+ * @Description:
+ */
+public class SpaceShip extends SpaceShipControls {
+    private String name;
+    public SpaceShip(String name){this.name = name;}
+
+    public String toString() {
+        return name;
+    }
+    public static void main(String[] args){
+        SpaceShip protector = new SpaceShip("NSEA Protector");
+        protector.forward(100);
+    }
+    //SpaceShip并非真正的SpaceShipControls类型，SpaceShipControls的所有方法在SpaceShip中都暴露了出来。代理解决了此难题
+}
---
+++ src/object2oriented/innerclass/Selector.java
@@ -0,0 +1,12 @@
+package object2oriented.innerclass;
+
+/**
+ * @Author: dyf
+ * @Date: 2019/5/5 16:54
+ * @Description:
+ */
+public interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
