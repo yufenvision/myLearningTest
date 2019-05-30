611bd25d3373ef98c0766a28923be8dd7366e3b4
yuf <duanyufeii@qq.com>
2019-05-30 22:36:47 +0800

    观察者模式Jdk内置

--- src/designmode/observer/jdkobserverable/WeatherData.java
+++ src/designmode/observer/jdkobserverable/WeatherData.java
@@ -8,5 +8,33 @@ import java.util.Observable;
  * @Description:
  */
 public class WeatherData extends Observable {
+    private float temperature;
+    private float humidity;
+    private float pressure;
 
+    public WeatherData() {}
+
+    public void measurementsChanged(){
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temp, float humidity, float pressure){//也可以实际的去调用外网数据，这里自己造的数据
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
 }
---
+++ src/designmode/observer/jdkobserverable/WeatherStationNew.java
@@ -0,0 +1,17 @@
+package designmode.observer.jdkobserverable;
+
+/**
+ * @Author: dyf
+ * @Date: 2019/5/30 22:28
+ * @Description:
+ */
+public class WeatherStationNew {
+    public static void main(String[] args){
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+
+        weatherData.setMeasurements(80,33, 32);
+
+    }
+
+}
