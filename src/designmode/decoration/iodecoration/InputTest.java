f051c14a28b494e093db413bce5cc49f9123143a
yuf <duanyufeii@qq.com>
2019-06-04 09:17:21 +0800

    io装饰

--- src/designmode/decoration/iodecoration/InputTest.java
+++ src/designmode/decoration/iodecoration/InputTest.java
@@ -1,7 +1,6 @@
-package designmode.decoration.beverage;
+package designmode.decoration.iodecoration;
 
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
