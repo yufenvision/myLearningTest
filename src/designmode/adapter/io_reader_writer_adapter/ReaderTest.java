bab0aca2666ea46d622a80ad15cbdc69f096bc21
yuf <duanyufeii@qq.com>
2019-06-17 21:52:17 +0800

    装饰模式

--- src/designmode/adapter/io_reader_writer_adapter/ReaderTest.java
+++ src/designmode/adapter/io_reader_writer_adapter/ReaderTest.java
@@ -9,14 +9,16 @@ import java.io.*;
  */
 public class ReaderTest {
     public static void main(String[] args){
-        try (InputStream in = ReaderTest.class.getResourceAsStream("file.txt")//这里的InputStream就是目标接口
+        try (InputStream in = ReaderTest.class.getResourceAsStream("file.txt")//这里的InputStream就是被适配接口（源角色）
         ){
-            Reader reader = new InputStreamReader(in);//Reader是源角色，InputStreamReader是适配器
+            Reader reader = new InputStreamReader(in);//Reader是目标接口，InputStreamReader是适配器
             char[] chars = new char[1024];
             reader.read(chars);
             System.out.println(new String(chars));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //对象适配器模式充满着良好的OO设计原则：使用对象组合，以修改的接口包装被适配者，被适配者的任何子类，都可以搭配着适配器使用
     }
 }
