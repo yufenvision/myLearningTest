691257a73fd3ea6dd0d3c3ce47f414672a0a8a47
yuf <duanyufeii@qq.com>
2019-07-01 00:22:40 +0800

    内部类

---
+++ src/thinkingInJava/innerclass/callbacksample/callbacktool/CallBack.java
@@ -0,0 +1,10 @@
+package thinkingInJava.innerclass.callbacksample.callbacktool;
+
+/**
+ * @Author: dyf
+ * @Date: 2019/7/1 00:06
+ * @Description: 回调接口
+ */
+public interface CallBack {
+    void excute();//实例对象执行某个方法
+}
