097f50428bab50a8366f879552430a3de7d337b2
yuf <duanyufeii@qq.com>
2019-07-22 22:35:48 +0800

    stream

---
+++ src/java2new/java8optional/lambda/Student.java
@@ -0,0 +1,55 @@
+package java2new.java8optional.lambda;
+
+/**
+ * @Author: dyf
+ * @Date: 2019/7/22 21:09
+ * @Description:
+ */
+
+public class Student {
+    private String name;
+    private int age;
+    private int height;
+
+    public Student(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
