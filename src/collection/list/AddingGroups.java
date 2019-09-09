63b6c14947fd58ed05b6b549d551801842160fdd
yuf <duanyufeii@qq.com>
2019-09-09 17:40:27 +0800

    上传list代码

--- src/collection/list/AddingGroups.java
+++ src/collection/list/AddingGroups.java
@@ -1,8 +1,6 @@
 package collection.list;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * @Author: dyf
@@ -14,5 +12,12 @@ public class AddingGroups {
         Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
         Integer[] moreInts = {6,7,8,9,10};
         collection.addAll(Arrays.asList(moreInts));
+        Collections.addAll(collection, 11, 12, 13, 14, 15);
+        Collections.addAll(collection, moreInts);
+        List<Integer> list = Arrays.asList(16, 17, 18, 19, 20);
+        list.set(1, 99);
+//        list.add(20);
+        System.out.println(collection);
+        System.out.println(list);
     }
 }
