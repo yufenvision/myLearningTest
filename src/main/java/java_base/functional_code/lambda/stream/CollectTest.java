111ca8e47dd5b1047d03f2acbe9abe6bc448ced5
SEELE <duanyufeii@qq.com>
2020-12-08 00:12:27 +0800

    stream去重

---
+++ src/main/java/java_base/functional_code/lambda/stream/CollectTest.java
@@ -0,0 +1,42 @@
+package java_base.functional_code.lambda.stream;
+
+import java_base.functional_code.lambda.Student;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @Author: dyf
+ * @Date: 2020/12/7 21:25
+ * @Description:
+ */
+public class CollectTest {
+
+    public static void main(String[] args) {
+        List<Student> students = Stream.of(
+                new Student("樱木花道", 16, 188),
+                new Student("樱木花道", 16, 180),
+                new Student("流川枫", 16, 187),
+                new Student("三井寿", 17, 175),
+                new Student("宫城良田", 16, 167),
+                new Student("赤木刚宪", 18, 192)).collect(Collectors.toList());
+
+        List<Student> distinctStudent = students.stream()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(o -> o.getName() + ";" + o.getAge()))), ArrayList::new));
+
+        List<Student> distinctStudent2 = students.stream().distinct().collect(Collectors.toList());
+        System.out.println(students);
+        System.out.println(distinctStudent);
+        System.out.println(distinctStudent2);
+
+        //自己写
+        Map<String, Student> map = new HashMap<>();
+        students.forEach(v -> map.put(v.getName() + ";" + v.getAge(), v));
+        List<Student> myList = map.values().stream().collect(Collectors.toList());
+        System.out.println(myList);
+    }
+
+
+}
