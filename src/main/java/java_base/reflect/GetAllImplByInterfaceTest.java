3111c4facc55c974884f2d03cdd331a05f233f4b
SEELE <duanyufeii@qq.com>
2020-12-20 11:26:08 +0800

    根据接口，获取全项目实现类与本包实现类功能完成

--- src/main/java/java_base/reflect/GetAllImplByInterfaceTest.java
+++ src/main/java/java_base/reflect/GetAllImplByInterfaceTest.java
@@ -17,32 +17,39 @@ import java.util.List;
 public class GetAllImplByInterfaceTest {
 
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args){
         Class<SomeService> superClz = SomeService.class;
-        String classPath = superClz.getClassLoader().getResource("").getPath();
-        System.out.println(classPath);
-        File file = new File(classPath);
-
-        List<String> allClzPath = getAllFileName(file, new ArrayList<>());
-        //获取所有class类对象
-        List<Class<?>> classes = new ArrayList<>();
-        allClzPath.stream().filter(v -> v.endsWith(".class"))
-                .map(v -> v.replaceAll(".*classes\\\\|\\.class",""))
-                .map(v -> v.replace("\\","."))
-                .forEach(v -> {
-//            System.out.println(v);
+        long start = System.currentTimeMillis();
+        //本包扫描实现类
+        getImplsByLocalScan(superClz).forEach(v -> {
             try {
-                Class clz = Class.forName(v);
-                if(superClz.isAssignableFrom(clz))classes.add(clz);
-            } catch (ClassNotFoundException e) {
+                System.out.println(v.newInstance().getClass());
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         });
-        classes.forEach(v -> System.out.println(v));
+        System.out.println("本包扫描耗时：" + (System.currentTimeMillis() - start));
+        //全项目扫描实现类
+
+        long two = System.currentTimeMillis();
+        List<Class<SomeService>> classes = getImplsByAllScan(superClz);
+        classes.forEach(v -> {
+            try {
+                System.out.println(v.newInstance());
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("全项目扫描耗时：" + (System.currentTimeMillis() - two));
 //        getAllDirectory(file, new LinkedList<>()).forEach(v -> System.out.println(v));
 
     }
 
+    //递归获取项目所有文件的全路径
     public static List<String> getAllFileName(File file, List<String> fileNames){
         if(file.isFile()){//文件
             fileNames.add(file.getPath());
@@ -59,6 +66,37 @@ public class GetAllImplByInterfaceTest {
         return fileNames;
     }
 
+    public static <T>List<Class<T>> getAllSubClass(File file, Class<T> superClz){
+        List<Class<T>> classes = new ArrayList<>();
+        getAllFileName(file, new ArrayList<>()).stream().filter(v -> v.endsWith(".class"))
+                .map(v -> v.replaceAll(".*classes\\\\|\\.class",""))
+                .map(v -> v.replace("\\","."))
+                .forEach(v -> {
+                    try {
+                        Class clz = Class.forName(v);
+                        if(superClz.isAssignableFrom(clz))classes.add(clz);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+        classes.remove(superClz);
+        return classes;
+    }
+
+    public static <T>List<Class<T>> getImplsByAllScan(Class<T> superClz){
+        if(!superClz.isInterface())return Collections.EMPTY_LIST;
+        String classPath = superClz.getClassLoader().getResource("").getPath();
+        return getAllSubClass(new File(classPath), superClz);
+    }
+
+    public static <T>List<Class<T>> getImplsByLocalScan(Class<T> superClz){
+        if(!superClz.isInterface())return Collections.EMPTY_LIST;
+        String classPath = superClz.getClassLoader().getResource("").getPath();
+        String packagePath = superClz.getPackage().getName().replace(".","/");
+        return getAllSubClass(new File(classPath + packagePath), superClz);
+    }
+
+    //尝试自己拼接文件全路径
     public static LinkedList<String> getAllDirectory(File file, LinkedList<String> directoryNames){
         if(file.isDirectory()){//文件夹
             String parentFileName = directoryNames.size() != 0 ? directoryNames.getLast() + "." : "";
@@ -73,24 +111,6 @@ public class GetAllImplByInterfaceTest {
         }
         return directoryNames;
     }
-
-    public <T>List<Class<? extends T>> getAllSubClassObjBySuperClass(Class<T> superClz){
-        if(!superClz.isInterface())return Collections.EMPTY_LIST;
-        String classPath = superClz.getClassLoader().getResource("").getPath();
-
-        File[] files = new File(classPath).listFiles();
-        for (File file : files) {
-
-        }
-
-        List<Class<? extends T>> allSubClzs = new ArrayList<>();
-        //判断是否是superClz实现类，是的话放入
-
-        return null;
-    }
-
-
-
 
 
 }
