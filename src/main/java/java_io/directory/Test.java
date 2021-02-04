583e432ed142b2a73b58cb70da2ee8307e0ca499
yuf <oncwnuAEyef8VMfTrUyVlVDYp0qM@git.weixin.qq.com>
2021-02-04 22:22:29 +0800

    递归三要素

--- src/main/java/java_io/directory/Test.java
+++ src/main/java/java_io/directory/Test.java
@@ -1,6 +1,7 @@
 package java_io.directory;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,20 +11,21 @@ import java.util.List;
  */
 public class Test {
 
+    static List<File> fileList = new ArrayList<>();
+
     public static void main(String[] args){
-//        List<File> fileList = new ArrayList<>();
-//        PPrint.pprint(getAllFile(new File("D:\\nfs\\media"), ".*", fileList));
+
+        PPrint.pprint(getAllFile(new File("D:\\nfs\\media"), ".*"));
 //          MakeDirectories.main(new String[]{"-d","E:/xxxx"});
         System.out.println(f(4));
     }
     //自己写的获取当前文件目录下的所有文件
-    public static List<File> getAllFile(File file, String regex, List<File> fileList){
+    public static List<File> getAllFile(File file, String regex){
         for (File item : file.listFiles()) {
-            if(item.isDirectory())
-                getAllFile(item, regex, fileList);
-            else{
-                if(item.getName().matches(regex))//正则匹配过滤
-                    fileList.add(item);
+            if(item.isFile()){
+                if(item.getName().matches(regex))fileList.add(item); //正则匹配过滤
+            }else{
+                getAllFile(item, regex);
             }
         }
         return fileList;
