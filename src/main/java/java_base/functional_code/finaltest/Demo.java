e8361336003861a3089898ab599cbd460f372217
yuf <oncwnuAEyef8VMfTrUyVlVDYp0qM@git.weixin.qq.com>
2021-03-08 22:48:15 +0800

    三个线程循环打印ABC，原子类和wait,notify

---
+++ src/main/java/java_base/functional_code/finaltest/Demo.java
@@ -0,0 +1,33 @@
+package java_base.functional_code.finaltest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @Author: dyf
+ * @Date: 2021/3/8 22:27
+ * @Description: lambda使用外部变量必须为final为什么？
+ */
+public class Demo {
+
+    private static Integer num = 0;
+
+    private static List<Integer> list = Arrays.asList(new Integer[]{1, 2, 3});
+
+    static Consumer consumer = (v) -> {
+        System.out.println(v);
+        num = 2;
+        System.out.println(num);
+    };
+
+    static void doSome(){
+        list.forEach(v -> num = num + v);
+    }
+
+    public static void main(String[] args) {
+        consumer.accept(num);
+
+    }
+
+}
